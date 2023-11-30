098e21d763553388378b6da32e4e21dfc105a641
Yanis Giboire <yanisgiboire@gmail.com>
2023-11-30 22:21:03 +0100

    Fix plugin/ping

--- plugins/ping/main.py
+++ plugins/ping/main.py
@@ -5,17 +5,15 @@ from pathlib import Path
 import contextlib
 import json
 
-PREFIX = '='
-
 class Ping(commands.Bot):
 
-    def __init__(self, bot: commands.Bot)->None:
-        super().__init__(command_prefix=PREFIX, intents=discord.Intents.all())
-
-    async def getPing():
-        ping = bot.latency * 1000
-        await ctx.send(f'Ping du bot -> {round(ping)}ms !')
-
-
-async def setup(bot: commands.Bot)->None:
-    await bot.add_cog(Ping(bot))
+    @commands.command(name='getPing')
+    async def get_ping(self, ctx, *args):
+        ping = self.bot.latency * 1000
+       
+        #|---------Ping---------|
+        embed = discord.Embed(
+            resPing=f'Ping du bot -> {round(ping)}ms !'
+        )
+        await self.channel.send( embed=embed)
+            
