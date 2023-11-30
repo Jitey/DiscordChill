bbf02448b322754554f2b43b2a78efacb2219303
Yanis Giboire <yanisgiboire@gmail.com>
2023-11-30 23:09:22 +0100

    Change @command.command to ping

--- plugins/ping/main.py
+++ plugins/ping/main.py
@@ -1,24 +1,22 @@
 import discord
 from discord.ext import commands
 
-from pathlib import Path
-import contextlib
-import json
+class Ping(commands.Cog):
 
-class Ping(commands.Bot):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
 
-    @commands.hybrid_command() (name='ping')
-    async def get_ping(self, ctx, *args):
+    @commands.command(name='ping')
+    async def get_ping(self, ctx):
         ping = self.bot.latency * 1000
-       
-        #|---------Ping---------|
+
+        # Créez un objet Embed pour le message
         embed = discord.Embed(
-            title =f'Ping du bot -> {round(ping)}ms !'
+            title=f'Ping du bot -> {round(ping)}ms !'
         )
-        await self.channel.send(embed=embed)
-        
 
-async def setup(bot: commands.Bot)->None:
-    await bot.add_cog(Ping(bot))
+        # Envoyez le message avec l'objet Embed
+        await ctx.send(embed=embed)
 
-    
+def setup(bot: commands.Bot) -> None:
+    bot.add_cog(Ping(bot))
