1b45248e5cec8b03e0a0f2bade0001e078b7769a
Jitey <jt.bousseau@gmail.com>
2023-12-04 22:18:04 +0100

    je vais pt un plomb

--- plugins/hotreload/main.py
+++ plugins/hotreload/main.py
@@ -70,9 +70,9 @@ class HotReload(commands.Cog):
                 last_commit = repo.head.commit
                 ic(last_commit.committed_datetime > last_commit_saved)
                 if last_commit.committed_datetime > last_commit_saved:
-                    repo.git.execute('git reset --hard origin/main') 
-                    repo.git.pull() 
                     ic("pull")
+                    # repo.git.execute('git reset --hard origin/main') 
+                    repo.git.pull() 
                     with open(f"{parent_folder}/save.json", 'w') as f:
                         json.dump({'last_commit': f"{last_commit.committed_datetime}"}, f, indent=2)
                     logging.info("Pull réussi")
